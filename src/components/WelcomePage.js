54cec335003bdbcce14bb07f62dabed0f6425237
christinapassafaro <christinapassafaro@gmail.com>
2020-12-07 08:35:13 -0500

    updates

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -10,6 +10,7 @@ function WelcomePage(){
                 <p className='hey'>Hey ✌🏼</p>
                 <p className='hey'>I'm Christina Passafaro</p>
                 <p className='description'>A Fullstack Software Engineer with a creative and systematic approach to problem solving is strongly influenced through my experience as an information technician in the Navy for four years and my experience as a social media strategist for my local government.</p>
+                <p className='description'>Reach Out at: christinapassafaro@gmail.com</p>
                 <Link to='/aboutme'>
                     <button>Learn More</button>
                 </Link>
